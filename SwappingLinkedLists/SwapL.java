735712713ae88bd934aea67426ba5701c934b7c1
Ashwini V <92977685+ashwiniv6@users.noreply.github.com>
2022-04-04 21:10:53 +0530

    Swapping Linked Lists

--- SwappingLinkedLists/SwapL.java
+++ SwappingLinkedLists/SwapL.java
@@ -1,24 +1,14 @@
 package SwappingLinkedLists;
 /*
-
- **
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- **
+Explanation:
 
 The main problem is to determine the kth node from start and kth node from end .
 
-Find kth node from start . The kth node from start can be found out using simple iteration upto k
-Find kth node from end. The kth node from end can be found out using looping upto (total number of nodes) - k +1 ;
-Swap the values of these two nodes.
-(kth node from end. = (n - k + 1)th node from start)
-where n = total number of nodes in the list.
+1.Find kth node from start . The kth node from start can be found out using simple iteration upto k
+2.Find kth node from end. The kth node from end can be found out using looping upto (total number of nodes) - k +1 ;
+3.Swap the values of these two nodes.
+     (kth node from end. = (n - k + 1)th node from start)
+     where n = total number of nodes in the list.
  */
 public class SwapL {
     /**
@@ -70,5 +60,29 @@ public class SwapL {
             return head;
         }
 
-    
+
 }
+
+/*
+Problem Statement: Swapping Nodes in a Linked List
+
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+
+
+Constraints:
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+ */
