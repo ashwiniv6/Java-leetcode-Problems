21c9cf4fc4913b586734f89b143f16655ef654b8
Ashwini V <92977685+ashwiniv6@users.noreply.github.com>
2022-04-04 21:12:35 +0530

    Swapping Linked Lists

--- SwappingLinkedLists/SwapL.java
+++ SwappingLinkedLists/SwapL.java
@@ -1,15 +1,30 @@
 package SwappingLinkedLists;
+
+
 /*
-Explanation:
+Problem Statement: Swapping Nodes in a Linked List
 
-The main problem is to determine the kth node from start and kth node from end .
+You are given the head of a linked list, and an integer k.
 
-1.Find kth node from start . The kth node from start can be found out using simple iteration upto k
-2.Find kth node from end. The kth node from end can be found out using looping upto (total number of nodes) - k +1 ;
-3.Swap the values of these two nodes.
-     (kth node from end. = (n - k + 1)th node from start)
-     where n = total number of nodes in the list.
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+
+
+Example 1:
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+
+Example 2:
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+
+
+Constraints:
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
  */
+
 public class SwapL {
     /**
      * Definition for singly-linked list.
@@ -64,25 +79,13 @@ public class SwapL {
 }
 
 /*
-Problem Statement: Swapping Nodes in a Linked List
+Explanation:
 
-You are given the head of a linked list, and an integer k.
+The main problem is to determine the kth node from start and kth node from end .
 
-Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
-
-
-
-Example 1:
-Input: head = [1,2,3,4,5], k = 2
-Output: [1,4,3,2,5]
-
-Example 2:
-Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
-Output: [7,9,6,6,8,7,3,0,9,5]
-
-
-Constraints:
-The number of nodes in the list is n.
-1 <= k <= n <= 105
-0 <= Node.val <= 100
+1.Find kth node from start . The kth node from start can be found out using simple iteration upto k
+2.Find kth node from end. The kth node from end can be found out using looping upto (total number of nodes) - k +1 ;
+3.Swap the values of these two nodes.
+     (kth node from end. = (n - k + 1)th node from start)
+     where n = total number of nodes in the list.
  */
